18643b4ac75ef7df8f5777ea223483d21e67ad93
wolfchkov <wolfchkovaj@gmail.com>
2018-05-29 22:08:56 +0300

    Update Task2.java
    Решить математически

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Task2 {
 
+    /**
+    Эту задачу нужно решить математически. 
+    Без преобразования числа в строку.
+    Делим на 10000. Получаем первое число. Вычитаем из исходного числа результат полученной цифры умноженной на 10000.
+    и так далее...
+    **/
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
