4d1c7f5df252df6826d30e8536a8c9110d447538
lisitski <lisitski.olga@gmail.com>
2018-05-29 13:02:58 +0300

    task3

---
+++ src/main/java/Task3.java
@@ -0,0 +1,11 @@
+public class Task3 {
+    public static void main(String[] args)
+
+    {
+        vector(1, 0, 0);
+    }
+    public static void vector (int X, int Y, int Z)
+    {
+        System.out.println("Result: " +(Math.sqrt( X * X + Y * Y + Z * Z )));
+    }
+}
