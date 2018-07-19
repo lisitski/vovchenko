b323c81dc73a82299c0fe8185d0da0d12233ff46
lisitski <lisitski.olga@gmail.com>
2018-07-19 21:10:38 +0300

    improved

--- src/main/java/home/vovchenko/Task.java
+++ src/main/java/home/vovchenko/Task.java
@@ -2,32 +2,32 @@ package home.vovchenko;
 
 public class Task {
     public static void main(String[] args) {
-        tasklong( 60, 80, 90, 120);
-        taskint(10,20,7,2);
-        taskfloat (5.6f,6.8f,8.8f, 8.4f);
-        taskdouble (66.7,88.8,55.5,34.6);
+        taskLong( 60, 80, 90, 120);
+        taskInt(10,20,7,2);
+        taskFloat (5.6f,6.8f,8.8f, 8.4f);
+        taskDouble (66.7,88.8,55.5,34.6);
     }
 
     //имя метода с прописной буквы
-    public static void tasklong (long x, long y, long z, long n)
+    public static void taskLong (long x, long y, long z, long n)
     {
         System.out.println ("Long result: " + (x - ( y + 123) / z * n));
     }
 
     //имя метода с прописной буквы
-    public static void taskint (int x, int y, int z, int n)
+    public static void taskInt (int x, int y, int z, int n)
     {
         System.out.println ("Int result: " + ((x + (50 - y * z) / n) - x ));
     }
     
     //имя метода с прописной буквы
-    public static void taskfloat (float x, float y, float z, float n)
+    public static void taskFloat (float x, float y, float z, float n)
     {
         System.out.println ("Float result: " + (x / (y * (z - 1) - 564) + n ));
     }
 
     //имя метода с прописной буквы
-    public static void taskdouble (double x, double y, double z, double n)
+    public static void taskDouble (double x, double y, double z, double n)
     {
         System.out.println("Double result: " + ( -x / -y + z / (n + 15)));
     }
