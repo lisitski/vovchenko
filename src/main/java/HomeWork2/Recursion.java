12b6ea0753e4ae7d4d8c9cf9325061313d561936
lisitski <lisitski.olga@gmail.com>
2018-07-24 23:52:48 +0300

    first

---
+++ src/main/java/HomeWork2/Recursion.java
@@ -0,0 +1,29 @@
+package HomeWork2;
+
+import java.util.Scanner;
+
+/*Сначала единственным программистом в компании был ее основатель, а каждый
+следующий программист, которого нанимали на работу, проходил по одному
+собеседованию с каждым из программистов, которые уже работали в компании на тот
+момент. Зная текущее количество программистов (вводится пользователем), определите,
+сколько всего собеседований было проведено в стенах компании.*/
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println("Enter number of developers: ");
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        int a = fact(value);
+        System.out.println("Quantity of interview: " + a);
+
+    }
+
+    static int fact(int n) {
+        int result;
+
+        if (n == 1)
+            return 1;
+
+        result = fact(n - 1) * n;
+        return result;
+    }
+}
